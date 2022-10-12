5ce7293db798f23e7962dfcaeeb9ebea48741a6e
XxLuisFer15xX <luisfer.sm15@gmail.com>
2022-10-12 17:02:28 -0600

    feat: Implement JSON WEB TOKENS.

--- routes/token.js
+++ routes/token.js
@@ -1,15 +1,54 @@
 import { Router } from 'express';
 import authByEmailPwd from '../helpers/authByEmailPwd.js';
+import jwt from 'jsonwebtoken';
+import * as fs from 'fs';
+import { USERS_BBDD } from '../bbdd.js';
 
 const authTokenRouter = Router();
 
-authTokenRouter.post('/login', (req, res) => {
+authTokenRouter.post('/login', async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) return res.sendStatus(400);
   try {
     const user = authByEmailPwd(email, password);
-    return res.send(`Usuario ${user.name} autenticado`);
+    const payload = {
+      guid: user.guid,
+      role: user.role,
+    };
+    const jwtOptions = {
+      algorithm: 'RS256',
+      expiresIn: '10m',
+    };
+    const privateKey = fs.readFileSync('./certs/private.key');
+    const token = jwt.sign(
+      payload,
+      { key: privateKey, passphrase: process.env.JWT_PASSPHRASE },
+      jwtOptions,
+    );
+    return res.send({
+      message: 'Token obtenido',
+      data: { token },
+    });
   } catch (error) {
+    return res.sendStatus(401);
+  }
+});
+
+authTokenRouter.get('/profile', (req, res) => {
+  const { token } = req.query;
+  if (!token) return res.sendStatus(401);
+  const publicKey = fs.readFileSync('./certs/public.key');
+  const jwtOptions = {
+    algorithms: ['RS256'],
+  };
+  try {
+    const userToken = jwt.verify(token, publicKey, jwtOptions);
+    const user = USERS_BBDD.find(user => user.guid === userToken.guid);
+    if (!user) return res.sendStatus(401);
+    delete user.password;
+    return res.send(user);
+  } catch (error) {
+    console.log(error);
     return res.sendStatus(401);
   }
 });
